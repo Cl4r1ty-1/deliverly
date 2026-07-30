59d0c120cce5136cbc79e637757c0b3d0b274d25
Cl4r1ty-1 <136800640+Cl4r1ty-1@users.noreply.github.com>
2026-07-30 10:25:25 +0800

    oops

--- src/app.py
+++ src/app.py
@@ -1,8 +1,34 @@
 import sqlite3
 import csv
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+cx = sqlite3.connect("records.db")
+cu = cx.cursor()
+
+def inital_db():
+    #clear all db records
+    cu.execute("DROP TABLE IF EXISTS Customer")
+    cu.execute("DROP TABLE IF EXISTS Restaurant")
+    cu.execute("DROP TABLE IF EXISTS Dish")
+    cu.execute("DROP TABLE IF EXISTS Orders")
+    cu.execute("DROP TABLE IF EXISTS OrdersItems")
+
+    #create new tables
+    cu.execute("""
+        CREATE TABLE Customer(
+            CustomerID INTEGER PRIMARY KEY,
+            CustomerName TEXT NOT NULL,
+            CustomerEmail TEXT NOT NULL,
+            CustomerAddress TEXT NOT NULL,
+            Suburb TEXT NOT NULL,
+            PostCode INTEGER NOT NULL,
+            CustomerPhone TEXT NOT NULL
+        )
+""")
+    
 
 @app.route("/")
 def home_message():
